adbfadb0be4fa84c81bb1d3567eb1f24111069af
elias <elias.ayoub@rubikloud.com>
2018-01-25 11:51:49 -0500

    initial upload of shareable jupyter notebook for measurement

---
+++ credentials_template.py
@@ -0,0 +1,10 @@
+login = {
+	'MFR' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MAT' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MCH' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MCZ' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MES' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MIT' : 'postgresql://<user>:<password>@<schema>/<db>',
+	'MSK' : 'postgresql://<user>:<password>@<schema>/<db>'
+
+}
